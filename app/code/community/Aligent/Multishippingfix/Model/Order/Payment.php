fdc881bc3f731699f2e41f56ff168682f07bea18
Luke Mills <luke@aligent.com.au>
2012-11-22 09:59:21 +1030

    Altered Payment::capture() to call the parent method on a recursive call.

--- app/code/community/Aligent/Multishippingfix/Model/Order/Payment.php
+++ app/code/community/Aligent/Multishippingfix/Model/Order/Payment.php
@@ -12,19 +12,23 @@
  */
 class Aligent_Multishippingfix_Model_Order_Payment extends Mage_Sales_Model_Order_Payment {
 
+    private static $_inMultishippingfixCapture = false;
+    
     public function capture($invoice) {
         // If this is a multishipping payment, then defer the capture until
         // the payment object has been cloned and added to the multishipping order.
         // Must do this, otherwise the capture occurs against the summary order
         // and the invoice is lost.
-        if (is_null($this->getParentOrderIncrementId())) {
+        if (self::$_inMultishippingfixCapture || is_null($this->getParentOrderIncrementId())) {
             return parent::capture($invoice);
         }
         return null;
     }
     
     public function multishippingfixCapture($invoice) {
+        self::$_inMultishippingfixCapture = true;
         return parent::capture($invoice);
+        self::$_inMultishippingfixCapture = false;
     }
     
 }
