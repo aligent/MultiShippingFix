7e57881dd2e236c779686acd5635632ab1f38d18
Luke Mills <luke@aligent.com.au>
2012-12-05 12:37:26 +1030

    Fixed capture method to use pay method for child payments. This prevents doing captures against the payment gateway for child payments.

--- app/code/community/Aligent/Multishippingfix/Model/Order/Payment.php
+++ app/code/community/Aligent/Multishippingfix/Model/Order/Payment.php
@@ -13,24 +13,27 @@
 class Aligent_Multishippingfix_Model_Order_Payment extends Mage_Sales_Model_Order_Payment {
 
     private static $_inMultishippingfixCapture = false;
-    
+
     public function capture($invoice) {
         // If this is a multishipping payment, then defer the capture until
         // the payment object has been cloned and added to the multishipping order.
         // Must do this, otherwise the capture occurs against the summary order
         // and the invoice is lost.
-        if (self::$_inMultishippingfixCapture || is_null($this->getParentOrderIncrementId())) {
+        if (self::$_inMultishippingfixCapture && !is_null($this->getParentOrderIncrementId()) && !is_null($invoice)) {
+            // capture() called for a child payment
+            $invoice->setIsPaid(true);
+            $this->pay($invoice);
+            return $this;
+        } elseif (is_null($this->getParentOrderIncrementId())) {
+            // capture() called for the summary payment
             return parent::capture($invoice);
         }
         return null;
     }
-    
+
     public function multishippingfixCapture($invoice) {
         self::$_inMultishippingfixCapture = true;
         return parent::capture($invoice);
         self::$_inMultishippingfixCapture = false;
     }
-    
 }
-
-?>
